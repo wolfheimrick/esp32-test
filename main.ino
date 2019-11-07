723b593c085ca07af5741522f94d0600328abf4e
wolf <wolfheimrick@gir.ovh>
2019-11-07 22:06:38 +0100

    display

--- main.ino
+++ main.ino
@@ -1,3 +1,10 @@
+//#include <Arduino.h>
+#include <U8x8lib.h>
+
+#ifdef U8X8_HAVE_HW_SPI
+#include <SPI.h>
+#endif
+
 #include <WiFi.h>
 #include <HTTPClient.h>
 
@@ -6,7 +13,12 @@ const char* password = "pineapple-pen-strawberry-785";
 
 const char* url = "https://smsapi.free-mobile.fr/sendmsg?&user=123456789&pass=PassFree&msg=sms";
 
+U8X8_SSD1306_128X64_NONAME_SW_I2C u8x8(/* clock=*/ 15, /* data=*/ 4, /* reset=*/ 16);
+
 void setup() {
+    u8x8.begin();
+    u8x8.setPowerSave(0);
+    u8x8.setFont(u8x8_font_chroma48medium8_r);
 
     Serial.begin(115200);
     delay(4000);
@@ -25,19 +37,25 @@ void setup() {
 }
 
 void loop() {
-      if (WiFi.status() == WL_CONNECTED) {
-          HTTPClient http;
-          http.begin(url);
-          int r = http.GET();
+    u8x8.setFont(u8x8_font_chroma48medium8_r);
+    if (WiFi.status() == WL_CONNECTED) {
+        HTTPClient http;
+        http.begin(url);
+        int r = http.GET();
 
-          if (r > 0) {
+        if (r > 0) {
             String payload = http.getString();
+            String stringR = String(r);
             Serial.println(r);
             Serial.println(payload);
-          } else {
+            u8x8.drawString(1,1, stringR.c_str());	// write something to the internal memory
+
+        } else {
+            u8x8.drawString(0,0, "Error on HTTP request");
             Serial.println("Error on HTTP request");
-          }
-          http.end();
-      }
-      delay(1000);
+        }
+        http.end();
+        u8x8.refreshDisplay();		// only required for SSD1606/7
+    }
+    delay(1000);
 }
